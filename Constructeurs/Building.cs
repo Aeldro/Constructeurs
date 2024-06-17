d308bef002cd1a34931e6a263d3789bec082f7b5
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-17 10:07:13 +0200

    Ajoutez des fichiers projet.

---
+++ Constructeurs/Building.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructeurs
+{
+    internal class Building
+    {
+        private double _height;
+        private int _floors;
+
+        public Building(int floors, double height)
+        {
+            this._floors = floors;
+            this._height = height;
+        }
+
+        public Building(int floors)
+        {
+            this._floors = floors;
+            this._height = floors * 3;
+        }
+
+        public double GetFloorMaxSize() { return _height / _floors; }
+
+        public int GetFloorCount() { return _floors; }
+
+        public double GetSize() { return _height; }
+    }
+}
---
+++ Constructeurs/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Constructeurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building firstBuilding = new Building(5, 6.8);
+            Building secondBuilding = new Building(4);
+
+            Console.WriteLine($"Nombre d'étages : {firstBuilding.GetFloorCount()} | Taille du bâtiment : {firstBuilding.GetSize()} | Taille de l'étage : {firstBuilding.GetFloorMaxSize()}");
+            Console.WriteLine($"Nombre d'étages : {secondBuilding.GetFloorCount()} | Taille du bâtiment : {secondBuilding.GetSize()} | Taille de l'étage : {secondBuilding.GetFloorMaxSize()}");
+        }
+    }
+}
